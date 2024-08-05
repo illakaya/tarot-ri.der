d1f4c9f032b5ef7fd68c597990a000d22066de5d
illakaya <mimijane.nguyen@gmail.com>
2024-08-05 19:52:34 +1000

    added comments

--- server/server.js
+++ server/server.js
@@ -1,16 +1,16 @@
 // Import Express server to create Node.js application
 const express = require("express");
-// Import Apollo server to communicate with Mongoose
+// Import Apollo server to handle GraphQL requests
 const { ApolloServer } = require("@apollo/server");
-// Import express middleware
+// Import express middleware to integrate Apollo Server with Express
 const { expressMiddleware } = require("@apollo/server/express4");
-// Import path (files and directories)
+// Import path to handle files and directory paths
 const path = require("path");
-// Import Authentication script
+// Import custom Authentication middleware
 const { authMiddleware } = require("./utils/auth");
-// Import the typeDefs and resolvers that define the models and process the data in GraphQL
+// Import GraphQL schema definitions and resolver functions
 const { typeDefs, resolvers } = require("./schemas");
-// Connect to MongoDB and Mongoose ODM
+// Connect to MongoDB and Mongoose ODM (located in config folder)
 const db = require("./config/connection");
 // Set up the port
 const PORT = process.env.PORT || 3001;
@@ -24,26 +24,25 @@ const server = new ApolloServer({
 
 // Create a new instance of an Apollo Server with the GraphQL schema
 const startApolloServer = async () => {
+  // Start Apollo Server
   await server.start();
-
+  // Middleware to parse URL-encoded and JSON data
   app.use(express.urlencoded({ extended: false }));
   app.use(express.json());
-
   // Serve up static assets
   app.use("/images", express.static(path.join(__dirname, "../client/images")));
-
+  // Apply Apollo middleware for GraphQL with authentication contect
   app.use("/graphql", expressMiddleware(server, {
     context: authMiddleware
   }));
-
+  // Server static files and routing in production
   if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../client/dist")));
-
     app.get("*", (req, res) => {
       res.sendFile(path.join(__dirname, "../client/dist/index.html"));
     });
   }
-
+  // Listen for the database connection to open, then start the Express server
   db.once("open", () => {
     app.listen(PORT, () => {
       console.log(`API server running on port ${PORT}!`);
