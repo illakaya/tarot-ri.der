d0ddf7ddf943f24503d0e859eb9c7aad23da41bc
illakaya <mimijane.nguyen@gmail.com>
2024-08-15 14:04:52 +1000

    fixed footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,14 +1,27 @@
 const Footer = () => {
   return (
     <footer>
-      <h1>Footer in the works</h1>
-            <h1>
-              <span role="img" aria-label="Face With Rolling Eyes Emoji">
-                🙄
-              </span>
-            </h1>
+      <h2>Join the Community</h2>
+      <h3>
+        <a href="/">
+          <i class="fab fa-facebook fa-2x"></i>
+        </a>
+        <a href="/">
+          <i class="fab fa-instagram fa-2x"></i>
+        </a>
+        <a href="/">
+          <i class="fab fa-twitter fa-2x"></i>
+        </a>
+        <a href="https://www.youtube.com/@mochipi9411">
+          <i class="fab fa-youtube fa-2x"></i>
+        </a>
+        <a href="https://github.com/illakaya/tarot-ri.der">
+          <i class="fab fa-github fa-2x"></i>
+        </a>
+        <p>Icons designed by <a href="https://www.freepik.com/">Freepik</a></p>
+      </h3>
     </footer>
-  )
+  );
 };
 
 export default Footer;
